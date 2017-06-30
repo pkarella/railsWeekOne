67f8e7f96368765f966037dbceb0d1d2959db4a1
peter <pkarella27@yahoo.com>
2017-06-30 09:09:45 -0700

    final create new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,18 @@ class ProductsController < ApplicationController
     @product = Product.new
     render :new
   end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country)
+  end
+
 end
